221fdc3ce100df212c91bb605cb17753abbf4603
Marcille Pierre-marie <root@DESKTOP-8MCCT48.localdomain>
2019-02-15 10:23:43 +0100

    setup

---
+++ fiche/creation.py
@@ -0,0 +1,4 @@
+#Le programme devra interagir avec tkinter et rcp pour permettre de centraliser chez le mj les fiches de perso#
+#On veillera donc a fournir toutes les entrées et sorties de ce programme selon un format qui sera rappelé en #
+#commentaire. Ce programme ne gère que l'initialisation des fiches de perso, et doit s'assurer que le résultat#
+#est non seulement correctement typé, mais aussi qu'il correspond aux limites théoriques du jeu. Enjoy. Nyaaaa#
