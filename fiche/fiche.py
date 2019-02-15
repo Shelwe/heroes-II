241292c1c1951a0beec80feeab89a6cac07e30a8
PM <root@DESKTOP-8MCCT48.localdomain>
2019-02-15 10:38:55 +0100

    Ajout d'un fichier de coordination pour les types.

--- fiche/fiche.py
+++ fiche/fiche.py
@@ -1,4 +1,19 @@
+import memory
+from random import randint
+
 class perso():
-	def __init__(arg1, arg2):
-		perso.a1=arg1
-		perso.a2=arg2 #bien sur, c'est juste pour donner un exemple d'init, mais c'est un peu l'ordre d'idée pour partir sur de la POO
+	def __init__(self, ad=0, ap=0, armor=50, MR=50, HP, Mana=0, Energy=0, agility, status=[],):
+		self.ad=ad
+		self.ap=ap
+		self.armor=armor
+		self.MR=MR
+		self.HPmax=HP
+		self.Mana=Mana
+		self.En=Energy
+		self.ag=agility
+		self.sta=status
+
+
+	def liststatus(self):
+		return [memory.recognize(self.sta[i]) for i in self.sta]
+
